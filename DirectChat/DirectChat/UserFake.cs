14e475047ffd269915f8ed1ceeb42c2b8f664456
Tom-England <tomengland6679@gmail.com>
2022-04-30 17:07:51 +0100

    Initial Commit

---
+++ DirectChat/DirectChat/UserFake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectChat
+{
+    public class UserFake
+    {
+        public string Name { get; set; }
+        public UserFake(string _name)
+        {
+            Name = _name;
+        }
+    }
+}
---
+++ DirectChat/DirectChat/UserListModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+namespace DirectChat
+{
+    public class UserListModel
+    {
+        public ObservableCollection<UserFake> Users { get; set; } = new ObservableCollection<UserFake>();
+        public UserListModel()
+        {
+            Users.Add(new UserFake("Tom")) ;
+            Users.Add(new UserFake("Mark"));
+            Users.Add(new UserFake("Artur"));
+            Users.Add(new UserFake("Tracy"));
+            Users.Add(new UserFake("Joel"));
+            Users.Add(new UserFake("Nicki"));
+            Users.Add(new UserFake("Harley"));
+            Users.Add(new UserFake("Jarmo"));
+            Users.Add(new UserFake("Ilysa"));
+            Users.Add(new UserFake("Varjhon"));
+            Users.Add(new UserFake("Toso"));
+        }
+    }
+}
